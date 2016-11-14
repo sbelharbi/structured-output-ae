c4fc6591c4bdeaffa87206833f589327c416f460
Soufiane Belharbi <soufiane.belharbi@insa-rouen.fr>
2016-11-14 18:23:47 +0100

    correct path download data.

--- sop_embed/download_face_data.sh
+++ sop_embed/download_face_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Download the data
-wget  pagesperso.litislab.fr/~sbelharbi/publishedCode/2016/pr-15-facial-landmarks-sop/inout/data/face.tar.gz -P ../../inout/data/
+wget  pagesperso.litislab.fr/~sbelharbi/publishedCode/2016/pr-15-facial-landmarks-sop/inout/data/face.tar.gz -P ../inout/data/
 # Untar the files
-tar -zxvf ../../inout/data/face.tar.gz  -C ../../inout/data/
+tar -zxvf ../inout/data/face.tar.gz  -C ../inout/data/
